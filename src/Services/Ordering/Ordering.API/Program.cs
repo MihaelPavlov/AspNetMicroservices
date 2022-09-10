1624b5b3d77b30ad5c997dc8609faee86711cc1d
MihaelPavlov <57910640+MihaelPavlov@users.noreply.github.com>
2022-09-10 16:59:26 +0300

    Complete ordering api logic and containerized the orderdb with sqlServer and order.api

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,19 +1,26 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace Ordering.API
 {
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
+    using Ordering.API.Extensions;
+    using Ordering.Infrastructure.Persistence;
+
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args)
+                           .Build()
+                           .MigrateDatabase<OrderContext>((context, services) =>
+                           {
+                               var logger = services.GetService<ILogger<OrderContextSeed>>();
+                               OrderContextSeed
+                                   .SeedAsync(context, logger)
+                                   .Wait();
+                           })
+                           .Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
