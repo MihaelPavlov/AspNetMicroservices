40f3adc7e2c040f336fa1a2ba52c1b1de34ee541
MihaelPavlov <57910640+MihaelPavlov@users.noreply.github.com>
2022-09-10 21:35:05 +0300

    create rabbitmq communication between basket and order microservices

---
+++ src/Services/Ordering/Ordering.API/Mapping/OrderingProfile.cs
@@ -0,0 +1,14 @@
+﻿namespace Ordering.API.Mapping
+{
+    using AutoMapper;
+    using EventBus.Messages.Events;
+    using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+
+    public class OrderingProfile : Profile
+    {
+        public OrderingProfile()
+        {
+            CreateMap<CheckoutOrderCommand, BasketCheckoutEvent>().ReverseMap();
+        }
+    }
+}
